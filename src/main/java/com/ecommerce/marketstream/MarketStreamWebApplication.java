24125c7484e901393ee98f63df87e114a23f5ce6
renjiearlkurniawan <renjiearlkurniawan.yo@gmail.com>
2025-11-26 15:02:52 +0700

    Initial commit

---
+++ src/main/java/com/ecommerce/marketstream/MarketStreamWebApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.marketstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketStreamWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketStreamWebApplication.class, args);
+	}
+
+}
